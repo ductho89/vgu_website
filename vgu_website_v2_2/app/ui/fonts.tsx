a0eb2aab9ca3343bc318d0267a5769e4efc9846c
nguynnhthi <13033@student.vgu.edu.vn>
2024-06-20 12:39:33 +0700

    Delete vgu_website_v2_2

--- vgu_website_v2_2/app/ui/fonts.tsx
+++
@@ -1,5 +0,0 @@
-import { IBM_Plex_Sans, Lusitana } from 'next/font/google';
-
- 
-export const sans = IBM_Plex_Sans({  weight: ['400'], subsets: ['latin'] });
-export const lusitana = Lusitana({  weight: ['400'], subsets: ['latin'] });
