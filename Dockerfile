afe0556e8c5a7eb56d1eda9a51a46236741bbcd2
nguynnhthi <13033@student.vgu.edu.vn>
2024-07-24 01:50:07 +0700

    Fix npm run build issue

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM node:latest
 WORKDIR /app
 COPY package*.json ./
 RUN npm ci
-RUN npm install -g wait-on
 COPY . .
 
 EXPOSE 3000
-CMD ["sh", "start-directus.sh"]
+CMD ["npm", "start"]
--- start-directus.sh
+++ start-directus.sh
@@ -8,11 +8,11 @@ echo "Starting the dependent system..."
 
 # Wait for the dependent system to be ready
 echo "Waiting for the dependent system to be ready..."
-wait-on http://directus:8055
+
 
 # Run the Next.js build
 echo "Starting Next.js build..."
-npm run build
+# npm run build
 
 # Optionally, start the Next.js server after the build
-npm start
+# npm start
