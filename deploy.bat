ac9e2a50b6027cc6dfc8d234b4d0921c3995d840
ngocyolo12345 <tranguyenngoc2862002@gmail.com>
2024-07-25 21:41:41 +0700

    adding .bat to build and deploy

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @echo off
 set USERNAME=root
-set PASSWORD=m42:0EXu$fp5
+set PASSWORD=
 set HOSTS=172.16.2.212
 set SCRIPT="docker pull tranguyenngoc2862002/vgu-web-test:latest && docker run --name vgu-web-app -d -p 3000:3000 -v ~/app/public:/app/public tranguyenngoc2862002/vgu-web-test:latest"
 set STOP_SCRIPT="docker stop vgu-web-app && docker rm vgu-web-app"
@@ -10,10 +10,13 @@ for %%H in (%HOSTS%) do (
 	plink -ssh -l %USERNAME% -pw %PASSWORD% %%H %STOP_SCRIPT%
 	
 	@echo removing old public folder
-	plink -ssh -l %USERNAME% -pw %PASSWORD% %%H "rm -r app & mkdir app"
+	plink -ssh -l %USERNAME% -pw %PASSWORD% %%H "rm -r app & mkdir app & mkdir database"
 
 	@echo copy local public folder to ~/app/public on server
 	scp -r ./public/ %USERNAME%@%%H:./app
+
+	@echo copy local database folder to ~/database on server
+	scp -r ./database %USERNAME%@%%H:./database
 	
 	@echo Update and run vgu-web-test
 	plink -ssh -l %USERNAME% -pw %PASSWORD% %%H %SCRIPT%
